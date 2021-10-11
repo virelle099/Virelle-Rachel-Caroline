521f62688313d002b9e5ff8c4dfd00415ad48f0b
virelle099 <92210012+virelle099@users.noreply.github.com>
2021-10-11 07:13:20 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Tugas5_Inheritance
+{
+    public class Komisi : object
+    {
+        public string NamaDepan { get; }
+        public string NamaTerakhir { get; }
+        public string NOKTP { get; }
+        private decimal labaKotor;
+        private decimal rateKomisi;
+        //Constructor
+        public Komisi(string namaDepan, string namaTerakhir, string noKTP, decimal labaKotor,
+            decimal rateKomisi)
+        {
+            NamaDepan = namaDepan;
+            NamaTerakhir = namaTerakhir;
+            NOKTP = noKTP;
+            LabaKotor = labaKotor;
+            RateKomisi = rateKomisi;
+        }
+        public decimal LabaKotor
+        {
+            get
+            {
+                return labaKotor;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value,
+                        $"{nameof(LabaKotor)} harus >= 0");
+                }
+                labaKotor = value;
+            }
+        }
+        public decimal RateKomisi
+        {
+            get
+            {
+                return rateKomisi;
+            }
+            set
+            {
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(rateKomisi)} harus > 0 dan < 1");
+                }
+                rateKomisi = value;
+            }
+        }
+        //menghitung pembayaran komisi pegawai
+        public decimal Menghasilkan() => labaKotor * rateKomisi;
+        public override string ToString() =>
+        $"Komisi Pegawai: { NamaDepan } { NamaTerakhir}\n" +
+        $"Nomer KTP: {NOKTP}\n" +
+        $"Laba Kotor: {labaKotor:C}\n" +
+        $"Rate Komisi: {rateKomisi:F2}";
+    }
+}
---
+++ komisipegawai.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tugas5_Inheritance
+{
+    class KomisiPegawai
+    {
+        public static void Main()
+        {
+            var pegawai = new Komisi("Sue", "Jones", "222-22-2222", 10000.00M, .06M);
+            //menampilkan data pegawai
+            Console.WriteLine("Informasi karyawan diperoleh dengan properti dan metode: \n");
+            Console.WriteLine($"Nama Depan: {pegawai.NamaDepan}");
+            Console.WriteLine($"Nama Terakhir: {pegawai.NamaTerakhir}");
+            Console.WriteLine($"No KTP: {pegawai.NOKTP}");
+            Console.WriteLine($"Laba: {pegawai.LabaKotor:C}");
+            Console.WriteLine($"Rate Komisi: {pegawai.RateKomisi:F2}");
+            Console.WriteLine($"Menghasilkan: {pegawai.Menghasilkan():C}");
+            pegawai.LabaKotor = 5000.00M;
+            pegawai.RateKomisi = .1M;
+            Console.WriteLine("\nMemperbarui Informasi Pegawai diperoleh dari ToString:\n");
+            Console.WriteLine(pegawai);
+            Console.WriteLine($"Menghasilkan: {pegawai.Menghasilkan():C}");
+        }
+    }
+}
